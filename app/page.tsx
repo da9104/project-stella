1cba0800c45da9d44e12512625127c9e42ca6fed
Dami K <dmk9104@gmail.com>
2024-09-23 13:45:08 +0900

    Layout styling (기본레이아웃)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+"use client"
+import { useChat } from "ai/react";
+
 
 export default function Home() {
+  const {messages, input, handleInputChange, handleSubmit} = useChat({
+    api: '/api',
+  })
+
   return (
-   <div>
-    hello 
+   <div className="min-h-screen bg-neutral-300">
+    {messages.length !== 0 ? (
+      messages.map((message, i) => (
+        <div key={i} className="flex justify-center">
+          <div className="bg-white p-4 rounded-lg shadow-lg">
+            <p>{message}</p>
+          </div>
+        </div>
+      ))
+    ) : (
+      <div className="w-full flex justify-center pt-32">
+        <h1 className="font-bold text-3xl">
+          Please use the input filed.
+        </h1>
+      </div>
+    )}
+    
    </div>
   );
 }
