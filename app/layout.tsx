1f3d9f0e9067f6dd2099db49cc1497437fc1a4c9
Dami K <dmk9104@gmail.com>
2024-09-23 13:19:09 +0900

    initial commit.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="h-full">
+          {children}
+        </main>
       </body>
     </html>
   );
