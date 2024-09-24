8c7f4bfd1f6136cdd6e633b9dba8c68d97dc6d17
Dami K <dmk9104@gmail.com>
2024-09-24 10:56:24 +0900

    Prisma config

--- app/db.ts
+++ app/db.ts
@@ -1,11 +1,22 @@
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+import ws from 'ws';
 
 const globalForPrisma = global as unknown as {
      prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma
 }
+
