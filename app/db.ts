44a8753f805a4ce4026208d5ca9b140a13f1afe7
Dami K <dmk9104@gmail.com>
2024-09-24 11:20:04 +0900

    db.ts modified.

--- app/db.ts
+++ app/db.ts
@@ -8,16 +8,22 @@ const connectionString = process.env.DATABASE_URL || '';
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
 
-const globalForPrisma = global as unknown as {
-     prisma: PrismaClient | undefined
-}
+// const globalForPrisma = global as unknown as {
+//      prisma: PrismaClient | undefined
+// }
 // export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 export const prisma =  new PrismaClient({ 
     adapter 
 });
 
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
 if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma
+    if (!global.prisma) {
+        global.prisma = prisma;
+    }
 }
 
